0fe536462262d4749704223f4ecd7594dcc83a54
lzbgithubcode <1835064412@qq.com>
2017-06-28 21:24:13 +0800

    init

---
+++ LZBVideoPlayer/LZBVideoPlayer/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  LZBVideoPlayer
+//
+//  Created by zibin on 2017/6/28.
+//  Copyright © 2017年 Apple. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
